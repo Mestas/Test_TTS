ed6d0600f4614778f83b7e9f4280ac87f066837a
Mestas <52190560+Mestas@users.noreply.github.com>
2025-12-11 11:53:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests, streamlit as st
 # API = "https://tts-kokoro-onnx-fastapi.up.railway.app/generate-speech"
-API = "https://tts-kokoro-onnx-fastapi.up.railway.app/generate-speech"
+API = "https://kokoro-onnx-fastapi-production.up.railway.app"
 text = st.text_input("文字", "你好，私有 TTS")
 if st.button("合成"):
     r = requests.post(API, json={"text": text, "voice": "zf_001", "speed": 1.0})
